eab24e0ccc77f6df6146830873a605d8626a3196
DingoDev <randilea@outlook.com>
2022-08-17 13:36:55 -0600

    added validation logic and adjusted organization

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,13 +7,25 @@ const Header = () => {
       <Container>
         <Row>
           <Col xs lg="4">
-            {dragonImage()}
+            {
+              <img
+                className="dragonPic"
+                src={require("../Images/Dragon.jpg")}
+                alt="Blue Dragon"
+              />
+            }
           </Col>
           <Col xs lg="4">
-            {schedulerHeader()}
+            {<h2 className="header">The Dungeon Scheduler</h2>}
           </Col>
           <Col xs lg="4">
-            {swordImage()}
+            {
+              <img
+                className="swordPic"
+                src={require("../Images/Sword.jpg")}
+                alt="Sword"
+              />
+            }
           </Col>
         </Row>
       </Container>
@@ -23,28 +35,28 @@ const Header = () => {
 
 // Helper Functions:
 
-const schedulerHeader = () => {
-  return <h2 className="header">The Dungeon Scheduler</h2>;
-};
+// const schedulerHeader = () => {
+//   return;
+// };
 
-const dragonImage = () => {
-  return (
-    <img
-      className="dragonPic"
-      src={require("../Images/Dragon.jpg")}
-      alt="Blue Dragon"
-    />
-  );
-};
+// const dragonImage = () => {
+//   return (
+//     <img
+//       className="dragonPic"
+//       src={require("../Images/Dragon.jpg")}
+//       alt="Blue Dragon"
+//     />
+//   );
+// };
 
-const swordImage = () => {
-  return (
-    <img
-      className="swordPic"
-      src={require("../Images/Sword.jpg")}
-      alt="Sword"
-    />
-  );
-};
+// const swordImage = () => {
+//   return (
+//     <img
+//       className="swordPic"
+//       src={require("../Images/Sword.jpg")}
+//       alt="Sword"
+//     />
+//   );
+// };
 
 export default Header;
--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,68 +1,126 @@
+import { useState } from "react";
 import { Form, Button, Label, Col, Row } from "react-bootstrap";
 
-const loggingIn = () => {
+// const loggingIn = () => {
+//   return (
+//     <div>
+//       <Row>
+//         <Col>{loginForm()}</Col>
+//       </Row>
+//     </div>
+//   );
+// };
+
+const LoggingInForm = () => {
+  const [emailIsValid, setEmailIsValid] = useState(false);
+  const [passwordIsValid, setPasswordIsValid] = useState(false);
+
+  // Validating Input Fields:
+  const validateField = (e) => {
+    let fieldValue = e.target.value;
+    let fieldId = e.target.id;
+    switch (fieldId) {
+      case "formBasicEmail":
+        validateEmail(fieldValue);
+        break;
+      case "formBasicPassword":
+        validatePassword(fieldValue);
+        break;
+    }
+  };
+
+  // Validating Email:
+  const validateEmail = (fieldValue) => {
+    if (fieldValue.includes("@" && ".com", ".net")) {
+      setEmailIsValid(true);
+    } else {
+      setEmailIsValid(false);
+    }
+  };
+
+  // Validating Password:
+  const validatePassword = (fieldValue) => {
+    if (fieldValue === null || fieldValue.trim() === "") {
+      setPasswordIsValid(false);
+    } else {
+      setPasswordIsValid(true);
+    }
+  };
+
   return (
-    <div>
-      <Row>
-        <Col>{loginForm()}</Col>
-      </Row>
-    </div>
-  );
-};
-
-const loginForm = () => {
-  return (
-    <form className="loggingForm">
-      <div class="form-outline mb-4">
-        <input type="email" id="form2Example1" class="form-control" />
-        <label class="form-label" for="form2Example1">
-          Enter Email
-        </label>
-      </div>
-
-      <div class="form-outline mb-4">
-        <input type="password" id="form2Example2" class="form-control" />
-        <label class="form-label" for="form2Example2">
-          Enter Password
-        </label>
-      </div>
-
-      <div class="row mb-4">
-        <div class="col d-flex justify-content-center">
-          <div class="form-check">
-            <input
-              class="form-check-input"
-              type="checkbox"
-              value=""
-              id="rememberCheckbox"
-            />
-            <label class="form-check-label" for="rememberMe">
-              {" "}
-              Remember me{" "}
+    <Row>
+      <Col>
+        <Form className="loggingForm">
+          <div class="form-outline mb-4">
+            <input type="email" id="formBasicEmail" class="formEmail" />
+            <label
+              class="form-label"
+              for="form2Example1"
+              isInvalid={emailIsValid === false}
+              onBlur={(e) => {
+                validateField(e);
+              }}
+            >
+              Enter Email
             </label>
           </div>
-        </div>
 
-        <div class="col">
-          <a href="#!">Forgot your password, traveler?</a>
-        </div>
-      </div>
+          <div class="form-outline mb-4">
+            <input
+              type="password"
+              id="formBasicPassword"
+              class="formPassword"
+            />
+            <label
+              class="form-label"
+              for="form2Example2"
+              isInvalid={passwordIsValid === false}
+              onBlur={(e) => {
+                validateField(e);
+              }}
+            >
+              Enter Password
+            </label>
+          </div>
 
-      <Button
-        variant="outline-danger"
-        class="btn btn-danger mb-4"
-        // className="enterButton"
-      >
-        Enter
-      </Button>
+          <div class="row mb-4">
+            <div class="col d-flex justify-content-center">
+              <div class="form-check">
+                <input
+                  class="form-check-input"
+                  type="checkbox"
+                  value=""
+                  id="rememberCheckbox"
+                />
+                <label class="form-check-label" for="rememberMe">
+                  {" "}
+                  Remember me{" "}
+                </label>
+              </div>
+            </div>
 
-      <div class="text-center">
-        <p>
-          New to the game? <a href="#!">Register here.</a>
-        </p>
-      </div>
-    </form>
+            <div class="col">
+              <a href="#!">Forget your password, traveler?</a>
+            </div>
+          </div>
+
+          <Button
+            variant="outline-danger"
+            class="btn btn-danger mb-4"
+            // className="enterButton"
+          >
+            Enter
+          </Button>
+
+          <div class="text-center">
+            <p>
+              New to the game? <a href="#!">Register here.</a>
+            </p>
+          </div>
+        </Form>
+      </Col>
+    </Row>
   );
 };
 
-export default loggingIn;
+export default LoggingInForm;
