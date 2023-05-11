5960743f57bc72b0f2803e3401e501422612c705
DingoDev <randilea@outlook.com>
2023-05-11 15:30:40 -0600

    Added beginning logic for API and added data.json

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import "./App.css";
 import Header from "./components/Header";
 import Views from "./components/Views";
 import React from "react";
+// import data from "./data.json";
 
 function App() {
   return (
     <div className="App">
       <React.Fragment>
+        {/* {data.items.map((item) => (
+          <div key={item.id}>{item.name}</div>
+        ))} */}
         <Header />
         <Views />
       </React.Fragment>
