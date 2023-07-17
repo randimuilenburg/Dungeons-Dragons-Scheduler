3fa8cbd41cbf63f1bd8255a56b382cdc984cf7d5
DingoDev <randilea@outlook.com>
2023-07-17 17:23:50 -0600

    Added JSON and beginnings of static API use

--- server.js
+++ server.js
@@ -1,18 +1,49 @@
+// const express = require("express");
+// const path = require("path");
+
+// const app = express();
+// const port = 4000;
+
+// // Serve static files from the build FileSystemDirectoryEntry
+// app.use(express.static(path.join(__dirname, "build")));
+
+// // Handle requests and respond with the React app
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "build", "index.html"));
+// });
+
+// // Start the server
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}!`);
+// });
+
 const express = require("express");
-const path = require("path");
+const fs = require("fs");
 
 const app = express();
 const port = 4000;
 
-// Serve static files from the build FileSystemDirectoryEntry
-app.use(express.static(path.join(__dirname, "build")));
+app.get("/api/profiles", (req, res) => {
+  fs.readFile("./src/data.json", "utf8", (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send("Internal Server Error");
+    }
 
-// Handle requests and respond with the React app
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+    try {
+      const jsonData = JSON.parse(data);
+      return res.json(jsonData.players);
+    } catch (error) {
+      console.error(error);
+      return res.status(500).send("Internal Server Error");
+    }
+  });
 });
 
-// Start the server
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}!`);
+  console.log(`Server is running on port ${port}`);
 });
+
+// then: ping time
+// get a specific person by id
+// display a specific person by id
