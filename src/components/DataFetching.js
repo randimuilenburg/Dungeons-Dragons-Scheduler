92df6c211839b74bc96309697c598f816deef8ea
DingoDev <randilea@outlook.com>
2023-08-17 16:08:34 -0600

    Connected backend with frontend, fetch data

---
+++ src/components/DataFetching.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+const useFetch = require("./useFetch");
+const url = "<http://localhost:4000/api/profiles>";
+const { data, loading, error } = useFetch(url);
+
+const useFetch = (url) => {
+  const [data, setdata] = useState(null);
+  const [loading, setloading] = useState(true);
+  const [error, seterror] = useState("");
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        seterror(data.error);
+        setdata(data);
+        setloading(false);
+      });
+  }, [url]);
+
+  return console.log(data), { data, loading, error };
+};
+
+// next: fetch and display data
+
+export default useFetch;
