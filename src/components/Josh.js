7ba9a287b44d49c8c3ea7122eb820828db4be115
DingoDev <randilea@outlook.com>
2023-08-23 15:35:44 -0600

    Edited players

---
+++ src/components/Josh.js
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+
+const joshPage = () => {
+  return <h1>This is Josh's info page!</h1>;
+};
+
+export default joshPage;
--- src/components/Tyler.js
+++
@@ -1,7 +0,0 @@
-import React, { useState } from "react";
-
-const tylerPage = () => {
-  return <h1>This is Tyler's info page!</h1>;
-};
-
-export default tylerPage;
