cc104114348f3c94b02c9ff064e75716c0a3c563
DingoDev <randilea@outlook.com>
2023-08-31 16:58:39 -0600

    Stripped data models down to be manageable, will move them back when further along. Also added campaigns by id array to player model.

---
+++ src/data-models-on-hold.js
@@ -0,0 +1,193 @@
+// I was too eager. I built out the data models for all of my player before everything worked seamlessly
+// everywhere else, so I have created more stress and work for myself. I am stripping it back down to only
+// one player, one data model, and I will put the others back in when I know all the pieces I want. For
+// now, keeping it caught up is creating a headache. One step back, but it will let me take two step forward
+// (or I will at least take faster steps? Something like that).
+
+// {
+//     "id": "2",
+//     "personalInfo": {
+//       "name": "Spenser W",
+//       "contact": {
+//         "email": "spenseremail@gmail.com",
+//         "phone": "720-765-4321"
+//       }
+//     },
+//     "playerAvailability": {
+//       "monday": [],
+//       "tuesday": ["evening"],
+//       "wednesday": ["evening"],
+//       "thursday": ["evening"],
+//       "friday": ["evening"],
+//       "saturday": ["afternoon", "evening", "night"],
+//       "sunday": ["afternoon", "evening"]
+//     },
+//     "playerExperience": {
+//       "yearsPlaying": 2,
+//       "numberOfCampaignsPlayed": 3,
+//       "numberOfCurrentCampaigns": 0,
+//       "numberOfCampaignsAsDm": 2
+//     },
+//     "playerPreferences": {
+//       "preferredCampaignLengths": ["one-shot", "short", "medium"]
+//     },
+//     "security": {
+//       "password": "SpenserPassword"
+//     },
+//     "characters": [
+//       {
+//         "id": "1",
+//         "name": "Diego Ignatius Infernus",
+//         "nickname": "Diego",
+//         "information": {
+//           "race": "Tiefling",
+//           "class": "Paladan",
+//           "background": "Urchin",
+//           "skills": "Deception, Sleight of Hand, Stealth",
+//           "languages": "Infernal, Common",
+//           "alignment": "Chaotic Neutral"
+//         }
+//       }
+//     ]
+//   },
+//   {
+//     "id": "3",
+//     "personalInfo": {
+//       "name": "Janey R",
+//       "contact": {
+//         "email": "janeyemail@gmail.com",
+//         "phone": "720-000-0000"
+//       }
+//     },
+//     "playerAvailability": {
+//       "monday": ["evening"],
+//       "tuesday": ["evening"],
+//       "wednesday": ["evening"],
+//       "thursday": ["evening", "night"],
+//       "friday": [],
+//       "saturday": ["afternoon", "evening", "night"],
+//       "sunday": ["afternoon", "evening", "night"]
+//     },
+//     "playerExperience": {
+//       "yearsPlaying": "1",
+//       "numberOfCampaignsPlayed": "1",
+//       "numberOfCurrentCampaigns": "0",
+//       "numberOfCampaignsAsDm": "0"
+//     },
+//     "playerPreferences": {
+//       "preferredCampaignLengths": ["one-shot", "short", "medium"]
+//     },
+//     "security": {
+//       "password": "JaneyPassword"
+//     },
+//     "characters": [
+//       {
+//         "id": "1",
+//         "name": "Theodora Ironheart Stoneforge",
+//         "nickname": "Theo",
+//         "information": {
+//           "race": "Dwarf",
+//           "class": "Bard",
+//           "background": "Sage",
+//           "skills": "History, Performance, Survival",
+//           "languages": "Dwarvish, Common",
+//           "alignment": "Lawful Good"
+//         }
+//       }
+//     ]
+//   },
+//   {
+//     "id": "4",
+//     "personalInfo": {
+//       "name": "Josh M",
+//       "contact": {
+//         "email": "joshemail@gmail.com",
+//         "phone": "720-111-1111"
+//       }
+//     },
+//     "playerAvailability": {
+//       "monday": ["morning"],
+//       "tuesday": ["morning", "evening"],
+//       "wednesday": [],
+//       "thursday": [],
+//       "friday": ["morning", "evening", "night"],
+//       "saturday": ["morning", "afternoon", "evening", "night"],
+//       "sunday": []
+//     },
+//     "playerExperience": {
+//       "yearsPlaying": "4",
+//       "numberOfCampaignsPlayed": "6",
+//       "numberOfCurrentCampaigns": "1",
+//       "numberOfCampaignsAsDm": "0"
+//     },
+//     "playerPreferences": {
+//       "preferredCampaignLengths": ["short", "medium", "long"]
+//     },
+//     "security": {
+//       "password": "JoshPassword"
+//     },
+//     "characters": [
+//       {
+//         "id": "1",
+//         "name": "Finnian Thistlebrook Bristleburrow",
+//         "nickname": "Finn",
+//         "description": {
+//           "element": null
+//         },
+//         "information": {
+//           "race": "Halfling",
+//           "class": "Warlock",
+//           "background": "Noble",
+//           "skills": "Animal Handling, Arcana, Medicine",
+//           "languages": "Halfling, Common",
+//           "alignment": "Lawful Neutral"
+//         }
+//       }
+//     ]
+//   },
+//   {
+//     "id": "5",
+//     "personalInfo": {
+//       "name": "Tim S",
+//       "contact": {
+//         "email": "timemail@gmail.com",
+//         "phone": "720-555-5555"
+//       }
+//     },
+//     "playerAvailability": {
+//       "monday": ["evening"],
+//       "tuesday": ["evening"],
+//       "wednesday": ["evening"],
+//       "thursday": ["evening"],
+//       "friday": ["evening", "night"],
+//       "saturday": ["morning", "afternoon", "evening", "night"],
+//       "sunday": ["morning", "afternoon", "evening"]
+//     },
+//     "playerExperience": {
+//       "yearsPlaying": "4",
+//       "numberOfCampaignsPlayed": "5",
+//       "numberOfCurrentCampaigns": "1",
+//       "numberOfCampaignsAsDm": "2"
+//     },
+//     "playerPreferences": {
+//       "preferredCampaignLengths": ["one-shot", "medium", "long"]
+//     },
+//     "security": {
+//       "password": "TimPassword"
+//     },
+//     "characters": [
+//       {
+//         "id": "1",
+//         "name": "Aricen Silverthorne",
+//         "nickname": "Ari",
+//         "information": {
+//           "race": "Elf",
+//           "class": "Rogue",
+//           "background": "Criminal",
+//           "skills": "Insight, Investigation, Stealth",
+//           "languages": "Elvish, Common",
+//           "alignment": "Chaotic Evil"
+//         }
+//       }
+//     ]
+//   }
---
+++ src/data-template-json.js
@@ -0,0 +1,63 @@
+// TEMPLATES FOR DATA MODELS FOR PLAYER AND CAMPAIGN
+
+// user
+
+// {
+//     "id": "3",
+//     "personalInfo": {
+//       "name": "Janey R",
+//       "contact": {
+//         "email": "janeyemail@gmail.com",
+//         "phone": "720-000-0000"
+//       }
+//     },
+//     "playerAvailability": {
+//       "monday": ["evening"],
+//       "tuesday": ["evening"],
+//       "wednesday": ["evening"],
+//       "thursday": ["evening", "night"],
+//       "friday": [],
+//       "saturday": ["afternoon", "evening", "night"],
+//       "sunday": ["afternoon", "evening", "night"]
+//     },
+//     "playerExperience": {
+//       "yearsPlaying": "1",
+//       "numberOfCampaignsPlayed": "1",
+//       "numberOfCurrentCampaigns": "0",
+//       "numberOfCampaignsAsDm": "0"
+//     },
+//     "playerPreferences": {
+//       "preferredCampaignLengths": ["one-shot", "short", "medium"]
+//     },
+//     "security": {
+//       "password": "JaneyPassword"
+//     },
+//     "characters": [
+//       {
+//         "id": 1,
+//         "name": "Theodora Ironheart Stoneforge",
+//         "nickname": "Theo",
+//         "information": {
+//           "race": "Dwarf",
+//           "class": "Bard",
+//           "background": "Sage",
+//           "skills": "History, Performance, Survival",
+//           "languages": "Dwarvish, Common",
+//           "alignment": "Lawful Good"
+//         }
+//       }
+//     ]
+//   },
+
+//   campaign
+// {
+//     "id": "3",
+//     "numberOfPlayers": 3,
+//     "campaignPlayers": ["Spenser S", "Josh M", "Tim S"],
+//     "length": "short",
+//     "dungeonMaster": "Tim S",
+//     "meetingFrequency": "monthly",
+//     "meetingDay": "Saturday",
+//     "meetingTime": "night",
+//     "isActive": false
+//   }
