f32bbfeb399df47176ec7c6533e14307018dd428
DingoDev <randilea@outlook.com>
2023-10-17 19:20:20 -0600

    Images are on the way to being added

--- scratch.py
+++ scratch.py
@@ -1,6 +1,7 @@
 from PIL import Image
 import io
 import requests
+import json
 
 # Open an image file
 image = Image.open('/home/dingodev/dungeon-scheduler/src/Images/Randiplayerimg.jpg')
@@ -12,28 +13,18 @@ with io.BytesIO() as buffer:
 
 url = "http://localhost:4000/api/users/1/characters/1"
 
-# Convert binary image data to base64 (optional, for JSON payload)
-import base64
-base64_image = base64.b64encode(binary_data).decode('utf-8')
-
-body = {
-    "imageBinary": base64_image,
-    # Include other data as needed
-    # "name": "Rhubarb Bellator Skyshocker",
-    # "nickname": "Barb",
-    # "information": {
-    #     "race": "Dragonborn",
-    #     "class": "Barbarian",
-    #     "background": "Solider",
-    #     "skills": "Athletics, Intimidation, Perception",
-    #     "languages": "Draconic, Common",
-    #     "alignment": "Chaotic Good",
-    #     "inUse": True
-    # }
+# Create a dictionary to hold JSON data
+json_data = {
+    "isCool": True,
 }
 
-headers = {'Content-Type': 'application/json'}
+# Use 'multipart/form-data' to send both JSON data and the file
+data = {
+    'json': (None, json.dumps(json_data), 'application/json'),
+    'file': ('Randiplayerimg.jpg', binary_data, 'image/jpeg'),
+}
 
-r = requests.put(url, json=body, headers=headers)
+# Make a POST request with multipart/form-data
+response = requests.put(url, files=data)
 
-print(r.text)
+print(response.text)
--- server.js
+++ server.js
@@ -3,17 +3,32 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const grid = require("gridfs-stream");
+const multer = require("multer"); // Import multer
 
 const app = express();
 const port = 4000;
+
+/*
+TODO:
+1. Remove image upload logic from /api/users/:userId/characters/:characterId
+2. Make a new endpoint that accepts post requests to upload a characater image and ONLY a character image.
+   This endpoint should take in the image, write it to gridfs, and then user mongoose to update the character object
+   with the image location (use the same endpoint url as above just have it use a post request be different)
+3. Write a get endpoint that returns an image from gridfs based on the location you provide
+4. Come see Spenser
+
+*/
 
 app.use(
   cors({
     origin: "http://localhost:3000",
     optionsSuccessStatus: 200,
-  }),
-  bodyParser.json({ limit: "50mb" })
+  })
+  // bodyParser.json({ limit: "50mb" })
+  // express.json()
 );
+
+app.use(express.json());
 
 const mongoURI = "mongodb://admin:adminPassword@localhost:27017/admin";
 
@@ -22,69 +37,145 @@ mongoose.connect(mongoURI, {
   useUnifiedTopology: true,
 });
 
-const dungeonSchedulerDB = mongoose.connection.useDb("dungeon-scheduler");
+const mongoConn = mongoose.connection;
+const dungeonSchedulerDB = mongoConn.useDb("dungeon-scheduler");
 
-let gfs; // GridFS stream
+grid.mongo = mongoose.mongo;
+const gfs = grid(mongoConn);
 
-dungeonSchedulerDB.once("open", () => {
-  gfs = grid(dungeonSchedulerDB.db, mongoose.mongo);
-  gfs.collection("uploads");
-});
+// Set up Multer for handling file uploads
+const storage = multer.memoryStorage(); // Use in-memory storage
+const upload = multer({ storage: storage });
 
 function isNumericString(inputString) {
   return /^\d+$/.test(inputString);
 }
 
-// ... Your existing routes ...
+app.get("/api/users", (req, res) => {
+  dungeonSchedulerDB
+    .collection("users")
+    .find({})
+    .toArray((err, users) => {
+      if (err) {
+        console.error("Error fetching users:", err);
+        return res.status(500).send("Internal Server Error");
+      }
+      return res.json(users);
+    });
+});
 
-// Upload image using GridFS
-app.post("/api/upload", (req, res) => {
-  if (!req.files || Object.keys(req.files).length === 0) {
-    return res.status(400).json({ message: "No files were uploaded." });
+app.get("/api/users/:userId", (req, res) => {
+  const { userId } = req.params;
+  if (!isNumericString(userId)) {
+    return res
+      .status(400)
+      .send(
+        "Please select a URL with numeric character in order to see the corresponding profile."
+      );
   }
 
-  const file = req.files.file;
-
-  // Create a write stream to store the uploaded file in GridFS
-  const writeStream = gfs.createWriteStream({
-    filename: file.name,
-    mode: "w",
-    content_type: file.mimetype,
-    metadata: { someKey: "someValue" }, // Optional metadata
-  });
-
-  writeStream.on("close", (file) => {
-    return res.status(200).json({
-      file_id: file._id,
-      message: "File uploaded successfully.",
+  dungeonSchedulerDB
+    .collection("users")
+    .findOne({ id: userId }, (err, user) => {
+      if (err) {
+        return res.status(500).send("Internal server error");
+      } else {
+        if (!user) {
+          return res.status(404).send("User not found");
+        }
+        return res.json(user);
+      }
     });
-  });
-
-  writeStream.on("error", (error) => {
-    return res.status(500).json({ error });
-  });
-
-  file.pipe(writeStream);
 });
 
-// Retrieve image using GridFS
-app.get("/api/images/:imageId", (req, res) => {
-  const { imageId } = req.params;
-
-  gfs.files.findOne({ _id: mongoose.Types.ObjectId(imageId) }, (err, file) => {
-    if (err) {
-      return res.status(500).json({ error: err });
-    }
-
-    if (!file) {
-      return res.status(404).json({ message: "File not found" });
-    }
-
-    const readStream = gfs.createReadStream({ _id: file._id });
-    readStream.pipe(res);
-  });
+app.put("/api/users/:userId", (req, res) => {
+  const { userId } = req.params;
+  const user = req.body;
+  dungeonSchedulerDB
+    .collection("users")
+    .updateOne({ id: userId }, { $set: user }, (err, result) => {
+      if (err) {
+        return res.status(500).send("Internal Server Error: " + err);
+      } else {
+        if (result.matchedCount === 1) {
+          return res.json(result);
+        }
+        return res
+          .status(404)
+          .send("Something went wrong " + result.modifiedCount);
+      }
+    });
 });
+
+app.put(
+  "/api/users/:userId/characters/:characterId",
+  upload.single("file"),
+  (req, res) => {
+    const { userId, characterId } = req.params;
+    const updatedCharacterFields = req.body;
+    const updatedCharacterFiles = req.files;
+    let updatedCharacterImageId = null;
+
+    const updateFields = {};
+    for (const key in updatedCharacterFields) {
+      updateFields[`characters.$.${key}`] = updatedCharacterFields[key];
+    }
+
+    // Check if there's an image upload
+    if (updatedCharacterFiles) {
+      const updatedCharacterImage = req.files.file;
+
+      const writeStream = gfs.createWriteStream({
+        filename: updatedCharacterImage.name,
+      });
+
+      updatedCharacterImage.pipe(writeStream);
+
+      writeStream.on("close", (file) => {
+        updatedCharacterImageId = file._id;
+      });
+    }
+
+    if (updatedCharacterImageId) {
+      updatedFields[`characters.$.imageId`] = updatedCharacterImageId;
+    }
+
+    dungeonSchedulerDB.collection("users").updateOne(
+      { id: userId, "characters.id": Number(characterId) }, // Match user and character by ID
+      { $set: updateFields }, // Update the matched character
+      (err, result) => {
+        if (err) {
+          return res.status(500).send("Internal Server Error " + err);
+        } else {
+          if (result.matchedCount > 0) {
+            [];
+            return res.json(result);
+          }
+          return res.status(404).send("User or character not found");
+        }
+      }
+    );
+  }
+);
+
+app.get("/api/users/:userId/characters", (req, res) => {
+  const { userId } = req.params;
+
+  dungeonSchedulerDB
+    .collection("users")
+    .findOne({ id: userId }, (err, user) => {
+      if (err) {
+        return res.status(500).send("Internal Server Error: " + err);
+      } else {
+        return res.json(user.characters);
+      }
+    });
+});
+
+// app.put
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+// then: ping time
